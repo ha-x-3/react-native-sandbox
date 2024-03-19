937ed6f39a8a31cffe4a505214a2634eb8bfc044
Brittney Epperson <beppers87@hotmail.com>
2024-03-19 11:25:40 -0500

    Added web dependencies and customized icons/splash screen

--- bethanys/App.js
+++ bethanys/App.js
@@ -1,11 +1,39 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+  const pie1 = require('./assets/images/shutterstock_1528835303.jpg');
+  const [fontsLoaded] = useFonts({
+    'RobotoCondensed-Regular': require('./assets/fonts/RobotoCondensed-Regular.ttf'),
+    'Alkatra-Regular': require('./assets/fonts/Alkatra-Regular.ttf'),
+    'Alkatra-Bold': require('./assets/fonts/Alkatra-Bold.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <View style={styles.imageView}>
+        <Image source={pie1} style={styles.imageStyle}></Image>
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Bethany's Pie Shop</Text>
+        <Text style={styles.slogan}>The Greatest Pies in the World!</Text>
+      </View>
+      <StatusBar style="light"/>
     </View>
   );
 }
@@ -17,4 +45,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageView: {
+    flex: 5,
+  },
+  imageStyle: {
+    flex: 1,
+    ...Platform.select({
+      android: {
+        resizeMode: 'contain'
+      }, 
+      ios: {
+        resizeMode: 'contain'
+      },
+      default: {
+        resizeMode: 'contain'
+      }
+    }),
+  },
+  titleContainer: {
+    flex: 3,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: 'Alkatra-Bold'
+  },
+  slogan: {
+    fontSize: 20,
+    fontFamily: 'RobotoCondensed-Regular'
+  }
 });
+
+export default App;
