07c7d9ef55d27b067e72fb745375b7f83a576d69
Brittney Epperson <beppers87@hotmail.com>
2024-03-23 13:55:10 -0500

    Updated Homepage to fetch from News API

--- globo-app/Home.js
+++ globo-app/Home.js
@@ -1,14 +1,49 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View,
+     ActivityIndicator, FlatList, Image, TouchableWithoutFeedback } from "react-native";
 
-export default function Homepage() {
+export default function Homepage({navigation}) {
+    const [dataLoading, finishLoading] = useState(true);
+    const [newsData, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://newsapi.org/v2/everything?q=tech&apiKey=e552bdb09af94b17b6af4968d1805c9e')
+        .then((response) => response.json())
+        .then((json) => setData(json.articles))
+        .catch((error) => console.error(error))
+        .finally(() => finishLoading(false))
+    }, []);
+
+    const storyItem = ({item}) => {
+        return (
+            <TouchableWithoutFeedback
+                onPress={() => 
+                    navigation.navigate('NewsDetail', {url: item.url})
+                }
+            >
+                <View style={styles.listings}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Image 
+                        style={styles.thumbnail}
+                        source={{uri: item.urlToImage}}
+                    />
+                    <Text style={blurb}>{item.description}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    };
 
     return (
         <View style={styles.container}>
-            <Text>This will be the Home Page</Text>
+            {dataLoading ? <ActivityIndicator /> : (
+                <FlatList
+                    data={newsData}
+                    renderItem={storyItem}
+                    keyExtractor={(item) => item.url}
+                />
+            )}
         </View>
     );
-
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +51,27 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        width: '100%',
+        padding: 20
+    },
+    thumbnail: {
+        height: 100,
+        width: '98%'
+    },
+    listings: {
+        paddingTop: 15,
+        paddingBottom: 25,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1
+    },
+    title: {
+        paddingBottom: 10,
+        fontFamily: 'OpenSans',
+        fontWeight: 'bold'
+    },
+    blurb: {
+        fontFamily: 'OpenSans',
+        fontStyle: 'italic'
     }
 });
