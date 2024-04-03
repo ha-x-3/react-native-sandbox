7734d0b4eff8585f009987e2039f9975f1bade80
Brittney Epperson <beppers87@hotmail.com>
2024-04-03 13:17:37 -0500

    Added Youtube link to Footer

--- bethanys-v2/app/Footer.js
+++ bethanys-v2/app/Footer.js
@@ -1,84 +1,105 @@
-import { StyleSheet, View, Text, Dimensions, Platform, Image } from "react-native";
+import {
+	StyleSheet,
+	View,
+	Text,
+	Dimensions,
+	Platform,
+	Image,
+} from 'react-native';
 import { useRouter } from 'expo-router';
+import * as WebBrowser from 'expo-web-browser';
 
 const windowDimensions = Dimensions.get('window');
 const winHeight = windowDimensions.height;
 const smallLogo = require('../assets/bethanys-pie-shop-logo_extra-4-black.png');
 
+const bethanyTube = () => {
+	WebBrowser.openBrowserAsync(
+    'https://www.youtube.com/@pluralsight'
+  );
+};
+
 const Footer = () => {
+	const router = useRouter();
 
-    const router = useRouter();
-
-    return (
-      <View style={styles.footer}>
-        <Image source={smallLogo} style={styles.smallLogoStyle} />
-        <Text
-          style={styles.menu}
-          onPress={() => {
-            router.push("/About");
-          }}
-        >
-          ABOUT
-        </Text>
-        <Text
-          style={styles.menu}
-          onPress={() => {
-            router.push("/News");
-          }}
-        >
-          NEWS
-        </Text>
-        <Text style={styles.menu}>BLOG</Text>
-        <Text style={styles.menu}>YOUTUBE</Text>
-      </View>
-    );
+	return (
+		<View style={styles.footer}>
+			<Image
+				source={smallLogo}
+				style={styles.smallLogoStyle}
+			/>
+			<Text
+				style={styles.menu}
+				onPress={() => {
+					router.push('/About');
+				}}
+			>
+				ABOUT
+			</Text>
+			<Text
+				style={styles.menu}
+				onPress={() => {
+					router.push('/News');
+				}}
+			>
+				NEWS
+			</Text>
+			<Text style={styles.menu}>BLOG</Text>
+			<Text 
+        style={styles.menu}
+        onPress={bethanyTube}
+      >
+        YOUTUBE
+      </Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  footer: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#d77948",
-    height: 50,
-    width: "100%",
-    flexDirection: "row",
-    ...Platform.select({
-      android: {
-        position: "absolute",
-        top: winHeight - 50,
-      },
-      ios: {
-        position: "absolute",
-        top: winHeight - 50,
-      },
-      default: {
-        position: 'relative',
-        bottom: 0
-      }
-    })
-  },
-  smallLogoStyle: {
-    height: 30,
-    width: 30,
-    ...Platform.select({
-        android: {
-            marginLeft: 23
-        },
-        ios: {
-            marginLeft: 23
-        },
-        default: {
-            left: '-35%'
-        }
-    })
-  },
-  menu: {
-    paddingLeft: 25,
-    paddingRight: 25,
-    color: '#FFFFFF',
-    fontFamily: 'WorkSans-Regular',
-    fontWeight: '700'
-  }
+	footer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#d77948',
+		height: 50,
+		width: '100%',
+		flexDirection: 'row',
+		...Platform.select({
+			android: {
+				position: 'absolute',
+				top: winHeight - 50,
+			},
+			ios: {
+				position: 'absolute',
+				top: winHeight - 50,
+			},
+			default: {
+				position: 'relative',
+				bottom: 0,
+			},
+		}),
+	},
+	smallLogoStyle: {
+		height: 30,
+		width: 30,
+		...Platform.select({
+			android: {
+				marginLeft: 23,
+			},
+			ios: {
+				marginLeft: 23,
+			},
+			default: {
+				left: '-35%',
+			},
+		}),
+	},
+	menu: {
+		paddingLeft: 25,
+		paddingRight: 25,
+		color: '#FFFFFF',
+		fontFamily: 'WorkSans-Regular',
+		fontWeight: '700',
+	},
 });
 
 export default Footer;
